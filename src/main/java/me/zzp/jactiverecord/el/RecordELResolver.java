65cfd56f859a27e619d7edc70fe66313770cb89c
redraiment <redraiment@gmail.com>
2014-05-25 09:35:23 +0800

    在EL中能像访问普通JavaBean一样访问Record对象的属性

---
+++ src/main/java/me/zzp/jactiverecord/el/RecordELResolver.java
@@ -0,0 +1,82 @@
+package me.zzp.jactiverecord.el;
+
+import java.beans.FeatureDescriptor;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.el.ELContext;
+import javax.el.ELResolver;
+import me.zzp.ar.Record;
+
+public final class RecordELResolver extends ELResolver {
+  @Override
+  public Object getValue(ELContext context, Object base, Object property) {
+    if (base != null && base instanceof Record && property != null) {
+      context.setPropertyResolved(true);
+      Record record = (Record) base;
+      return record.get(property.toString());
+    } else {
+      context.setPropertyResolved(false);
+      return null;
+    }
+  }
+
+  @Override
+  public Class<?> getType(ELContext context, Object base, Object property) {
+    if (base != null && base instanceof Record && property != null) {
+      context.setPropertyResolved(true);
+      Record record = (Record) base;
+      Object o = record.get(property.toString());
+      return o == null? null: o.getClass();
+    } else {
+      context.setPropertyResolved(false);
+      return null;
+    }
+  }
+
+  @Override
+  public void setValue(ELContext context, Object base, Object property, Object value) {
+    if (base != null && base instanceof Record && property != null) {
+      context.setPropertyResolved(true);
+      Record record = (Record) base;
+      record.set(property.toString(), value);
+    } else {
+      context.setPropertyResolved(false);
+    }
+  }
+
+  @Override
+  public boolean isReadOnly(ELContext context, Object base, Object property) {
+    if (base != null && base instanceof Record && property != null) {
+      context.setPropertyResolved(true);
+      return property.equals("id");
+    } else {
+      context.setPropertyResolved(false);
+      return false;
+    }
+  }
+
+  @Override
+  public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext context, Object base) {
+    List<FeatureDescriptor> list = new ArrayList<>();
+    if (base != null && base instanceof Record) {
+      Record record = (Record) base;
+      for (String column : record.columnNames()) {
+        FeatureDescriptor feature = new FeatureDescriptor();
+        feature.setDisplayName(column);
+        feature.setName(column);
+        feature.setShortDescription(column);
+        feature.setHidden(false);
+        feature.setExpert(false);
+        feature.setPreferred(true);
+        list.add(feature);
+      }
+    }
+    return list.iterator();
+  }
+
+  @Override
+  public Class<?> getCommonPropertyType(ELContext context, Object base) {
+    return String.class;
+  }
+}
---
+++ src/main/java/me/zzp/jactiverecord/el/ResolverSetup.java
@@ -0,0 +1,18 @@
+package me.zzp.jactiverecord.el;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.jsp.JspApplicationContext;
+import javax.servlet.jsp.JspFactory;
+
+public class ResolverSetup implements ServletContextListener {
+  @Override
+  public void contextInitialized(ServletContextEvent e) {
+    JspApplicationContext context = JspFactory.getDefaultFactory().getJspApplicationContext(e.getServletContext());
+    context.addELResolver(new RecordELResolver());
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent e) {
+  }
+}
