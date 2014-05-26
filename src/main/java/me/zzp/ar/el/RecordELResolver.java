01043ce63903c157ac3f2c41487877a43e6231e7
redraiment <redraiment@gmail.com>
2014-05-26 11:20:15 +0800

    完善javadoc，简化包名

--- src/main/java/me/zzp/ar/el/RecordELResolver.java
+++ src/main/java/me/zzp/ar/el/RecordELResolver.java
@@ -1,4 +1,4 @@
-package me.zzp.jactiverecord.el;
+package me.zzp.ar.el;
 
 import java.beans.FeatureDescriptor;
 import java.util.ArrayList;
@@ -8,18 +8,36 @@ import javax.el.ELContext;
 import javax.el.ELResolver;
 import me.zzp.ar.Record;
 
+/**
+ * <p><code>me.zzp.ar.Record</code>的EL表达式解析器。
+ * 简化<code>Record</code>对象方法调用的方式。</p>
+ * @author redraiment
+ * @since 1.0
+ */
 public final class RecordELResolver extends ELResolver {
   private final boolean camelCase;
+
+  public RecordELResolver() {
+    this(false);
+  }
 
   public RecordELResolver(boolean camelCase) {
     this.camelCase = camelCase;
   }
-  
+
   private String getKey(Object property) {
     String key = property.toString();
     return camelCase? key.replaceAll("(?=[A-Z])", "_").toLowerCase(): key;
   }
 
+  /**
+   * <p>像访问普通JavaBean一样访问Record中的字段。</p>
+   * <p><code>${user.name}</code>等价于<code>${user.get("name")}</code>。</p>
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @param property 属性
+   * @return 返回相应属性的值
+   */
   @Override
   public Object getValue(ELContext context, Object base, Object property) {
     if (base != null && base instanceof Record && property != null) {
@@ -32,6 +50,13 @@ public final class RecordELResolver extends ELResolver {
     }
   }
 
+  /**
+   * 获取属性在数据库中相应的类型。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @param property 属性
+   * @return 返回相应属性的类型
+   */
   @Override
   public Class<?> getType(ELContext context, Object base, Object property) {
     if (base != null && base instanceof Record && property != null) {
@@ -45,6 +70,14 @@ public final class RecordELResolver extends ELResolver {
     }
   }
 
+  /**
+   * <p>设置属性值。</p>
+   * <p>等价于调用<code>Record#set(String name, Object value)</code>。</p>
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @param property 属性
+   * @param value 值
+   */
   @Override
   public void setValue(ELContext context, Object base, Object property, Object value) {
     if (base != null && base instanceof Record && property != null) {
@@ -56,17 +89,30 @@ public final class RecordELResolver extends ELResolver {
     }
   }
 
+  /**
+   * 均可写。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @param property 属性
+   * @return false
+   */
   @Override
   public boolean isReadOnly(ELContext context, Object base, Object property) {
     if (base != null && base instanceof Record && property != null) {
       context.setPropertyResolved(true);
-      return property.equals("id");
+      return false;
     } else {
       context.setPropertyResolved(false);
       return false;
     }
   }
 
+  /**
+   * 返回由列名组成的列表。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @return 包含当前表的所有列名
+   */
   @Override
   public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext context, Object base) {
     List<FeatureDescriptor> list = new ArrayList<FeatureDescriptor>();
@@ -86,6 +132,12 @@ public final class RecordELResolver extends ELResolver {
     return list.iterator();
   }
 
+  /**
+   * 属性为字符串类型。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @return String.class
+   */
   @Override
   public Class<?> getCommonPropertyType(ELContext context, Object base) {
     return String.class;
--- src/main/java/me/zzp/ar/el/TableELResolver.java
+++ src/main/java/me/zzp/ar/el/TableELResolver.java
@@ -1,7 +1,8 @@
-package me.zzp.jactiverecord.el;
+package me.zzp.ar.el;
 
 import java.beans.FeatureDescriptor;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import javax.el.ELContext;
@@ -9,7 +10,26 @@ import javax.el.ELResolver;
 import me.zzp.ar.Record;
 import me.zzp.ar.Table;
 
+/**
+ * <p><code>me.zzp.ar.Table</code>的EL表达式解析器。
+ * 简化<code>Table</code>对象方法调用的方式。</p>
+ * @author redraiment
+ * @since 1.0
+ */
 public final class TableELResolver extends ELResolver {
+  /**
+   * <p>支持<code>all</code>、<code>first</code>、<code>last</code>和索引四种查询方式：</p>
+   * <ul>
+   *   <li>all：调用<code>Table#all()</code>。即<code>${User.all}</code>等价于<code>${User.all()}</code></li>
+   *   <li>first：调用<code>Table#first()</code>。即<code>${User.first}</code>等价于<code>${User.first()}</code></li>
+   *   <li>last：调用<code>Table#last()</code>。即<code>${User.last}</code>等价于<code>${User.last()}</code></li>
+   *   <li>索引：调用<code>Table#find(int id)</code>。即<code>${User[1]}</code>等价于<code>${User.find(1)}</code></li>
+   * </ul>
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @param property 属性
+   * @return 返回相应的方法调用结果
+   */
   @Override
   public Object getValue(ELContext context, Object base, Object property) {
     if (base != null && base instanceof Table && property != null) {
@@ -34,11 +54,18 @@ public final class TableELResolver extends ELResolver {
     }
   }
 
+  /**
+   * 如果属性值为<code>all</code>，则返回 {@link java.util.List} 类型；
+   * 否则返回 {@link me.zzp.ar.Record} 类型。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @param property 属性
+   * @return 返回相应属性的类型
+   */
   @Override
   public Class<?> getType(ELContext context, Object base, Object property) {
     if (base != null && base instanceof Table && property != null) {
       context.setPropertyResolved(true);
-      Table table = (Table) base;
       if (property instanceof String) {
         if (property.equals("all")) {
           return List.class;
@@ -55,11 +82,25 @@ public final class TableELResolver extends ELResolver {
     }
   }
 
+  /**
+   * Table不允许修改。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @param property 属性
+   * @param value 值
+   */
   @Override
   public void setValue(ELContext context, Object base, Object property, Object value) {
     context.setPropertyResolved(base != null && base instanceof Table);
   }
 
+  /**
+   * 总是返回true。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @param property 属性
+   * @return true
+   */
   @Override
   public boolean isReadOnly(ELContext context, Object base, Object property) {
     if (base != null && base instanceof Table) {
@@ -71,12 +112,17 @@ public final class TableELResolver extends ELResolver {
     }
   }
 
+  /**
+   * 仅返回all、first和last三个方法名。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @return 包含all、first和last三个方法名
+   */
   @Override
   public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext context, Object base) {
     List<FeatureDescriptor> list = new ArrayList<FeatureDescriptor>();
     if (base != null && base instanceof Table) {
-      Table table = (Table) base;
-      for (String column : table.getColumns().keySet()) {
+      for (String column : Arrays.asList("all", "first", "last")) {
         FeatureDescriptor feature = new FeatureDescriptor();
         feature.setDisplayName(column);
         feature.setName(column);
@@ -90,6 +136,12 @@ public final class TableELResolver extends ELResolver {
     return list.iterator();
   }
 
+  /**
+   * 属性为字符串类型。
+   * @param context EL表达式上下文
+   * @param base Table对象
+   * @return String.class
+   */
   @Override
   public Class<?> getCommonPropertyType(ELContext context, Object base) {
     return String.class;
