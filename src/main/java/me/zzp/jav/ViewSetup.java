d703caaf56d793f2989cdfe94d45e03392c192c5
redraiment <redraiment@gmail.com>
2014-06-04 22:01:18 +0800

    View of Ruby on Rails in Java: 自动加载jsp文件并映射成view

---
+++ src/main/java/me/zzp/jav/ViewSetup.java
@@ -0,0 +1,56 @@
+package me.zzp.jav;
+
+import java.util.LinkedList;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+import org.apache.jasper.servlet.JspServlet;
+
+public class ViewSetup implements ServletContextListener {
+
+  private List<String> listJsp(ServletContext context, String root) {
+    List<String> files = new LinkedList<>();
+    for (String path : context.getResourcePaths(root)) {
+      if (path.endsWith(".jsp")) {
+        files.add(path);
+      } else if (path.endsWith("/")) {
+        files.addAll(listJsp(context, path));
+      }
+    }
+    return files;
+  }
+
+  @Override
+  public void contextInitialized(ServletContextEvent e) {
+    ServletContext context = e.getServletContext();
+    String paths = context.getInitParameter("jactionview-paths");
+    if (paths == null || paths.isEmpty()) {
+      return;
+    }
+
+    for (String root : paths.split(",")) {
+      if (!root.endsWith("/")) {
+        root = root.concat("/");
+      }
+
+      for (String file : listJsp(context, root)) {
+        ViewConfig view = new ViewConfig(context, file, root);
+        try {
+          JspServlet jsp = context.createServlet(JspServlet.class);
+          jsp.init(view);
+          Dynamic mapping = context.addServlet(view.getServletName(), jsp);
+          mapping.addMapping(view.getUrl());
+        } catch (ServletException ex) {
+          System.err.println(ex.getMessage());
+        }
+      }
+    }
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent e) {
+  }
+}
